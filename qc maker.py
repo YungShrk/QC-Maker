39c1206b132e9d0b5e66b8cea1e03da56121709f
YungShrk <61809790+YungShrk@users.noreply.github.com>
2023-03-02 22:08:42 -0800

    added .py file
    creates a txt file with everything you need!

---
+++ qc maker.py
@@ -0,0 +1,35 @@
+fileName = input("Enter name of show/feature: ")
+print(fileName)
+seasonNum = input("Enter number of seasons: ")
+print(seasonNum)
+epNum = input("Enter number of episodes: ")
+print(epNum)
+
+season = 0
+ep = 1
+line = ''
+
+def lines(fileName, line):
+    for x in range((len(fileName)+5)):
+        line = line + '-'
+    return line
+
+
+with open('C:\\Users\\wyatt\\OneDrive\\Documents\\Test\\'+fileName+'.txt', 'w') as f:
+   f.write(fileName + '\n' + lines(fileName,line))
+   for x in range(int(seasonNum)):
+    season = season + 1
+    ep = 1
+    for x in range(int(epNum)):
+        f.write('\n')
+        f.write('\n')
+        if (ep >= 10):
+           f.write(str(season) + str(ep))
+        if (ep < 10):   
+            f.write(str(season) + '0' + str(ep))
+        ep = ep + 1
+        f.write('\n')
+        f.write('\n')
+        f.write('\n')  
+        f.write(lines(fileName,line))                      
+        
